64d2fbd8a4a54fced13592c16c3f37fa6d378a66
Arthur Puyou <arthur@puyou.fr>
2012-06-29 00:49:25 +0200

    Là ça marche ^^

--- api.php
+++ api.php
@@ -5,13 +5,15 @@ session_start();
 
 $POSS = @new SoapClient($_CONF['soap_url'].'POSS2.class.php?wsdl');
 
+if(isset($_SESSION['cookies_mozart'])) {
+	$POSS->_cookies = $_SESSION['cookies_mozart'];
+}
+
 if(isset($_GET['method'])) {
 	$method = $_GET['method'];
 	echo json_encode(call_user_func_array(array($POSS,$method), $_POST));
 }
 
-if(isset($_SESSION['cookies_mozart'])) { 
-	$POSS->_cookies = $_SESSION['cookies_mozart'];
-} else {
+if(isset($POSS->cookies)) {
 	$_SESSION['cookies_mozart'] = $POSS->_cookies;
 }
