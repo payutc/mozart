90b5dd79c6d23bf944fbd961499eb4bd9b71ace6
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-28 23:06:01 +0200

    Maintient de la session soap

--- api.php
+++ api.php
@@ -1,8 +1,15 @@
 <?php
 
 include 'config.php';
+session_start();
 
 $POSS = @new SoapClient($_CONF['soap_url'].'POSS2.class.php?wsdl');
+
+if(isset($_SESSION['cookies'])) { 
+	$POSS->_cookies = $_SESSION['cookies'];
+} else {
+	$_SESSION['cookies'] = $POSS->_cookies;
+}
 
 if(isset($_GET['method'])) {
 	$method = $_GET['method'];
