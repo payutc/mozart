176ba4716644d80826f4b54862b21cd349ef1db8
Matthieu Guffroy <mattgu74@gmail.com>
2012-06-29 00:48:00 +0200

    [fix]

--- api.php
+++ api.php
@@ -5,14 +5,13 @@ session_start();
 
 $POSS = @new SoapClient($_CONF['soap_url'].'POSS2.class.php?wsdl');
 
-if(isset($_SESSION['cookies_mozart'])) { 
-	$POSS->_cookies = $_SESSION['cookies_mozart'];
-} else {
-	$_SESSION['cookies_mozart'] = $POSS->_cookies;
-}
-
 if(isset($_GET['method'])) {
 	$method = $_GET['method'];
 	echo json_encode(call_user_func_array(array($POSS,$method), $_POST));
 }
 
+if(isset($_SESSION['cookies_mozart'])) { 
+	$POSS->_cookies = $_SESSION['cookies_mozart'];
+} else {
+	$_SESSION['cookies_mozart'] = $POSS->_cookies;
+}
