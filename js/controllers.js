affe5a8abcab7575520ad58a9d1c484abdde8d70
kofronpi <kofronpi@etu.utc.fr>
2014-01-20 00:28:37 +0100

    registering application and key local storage

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var mozartApp =  angular.module('mozartApp', [], function($locationProvider) {
+var mozartApp =  angular.module('mozartApp', ['LocalStorageModule'], function($locationProvider) {
       $locationProvider.html5Mode(true);
     });
 
@@ -21,30 +21,57 @@ mozartApp.service('mrequest', function($http){
 });	
 
 
-mozartApp.controller('UserCtrl',function($scope, $http, $location, $window, mrequest){
+mozartApp.controller('UserCtrl',function($scope, $http, $location, $window, mrequest, localStorageService) {
 
-    //pas de ticket, redirection vers le CAS
-    if(!$location.search().ticket){    
-        $http.post(server_url + '/POSS3/getCasUrl').success(function(data) {
-            $window.location.href = angular.fromJson(data) + '/login?service=' + encodeURIComponent($location.absUrl());
-        });
-    }
-    //validation du ticket
-    else{
-        var ticket = $location.search().ticket ;
-        $location.search('');
-        mrequest.do('POSS3', 'loginCas', {ticket: ticket, service: $location.absUrl()}).success(function(data){
-            });
 
-        mrequest.do('POSS3', 'getStatus', {}).success(function(data){
-            $scope.username = data.user_data.firstname +' '+ data.user_data.lastname;
+            //No ticket, redirection to CAS
+            if(!$location.search().ticket){    
+                $http.post(server_url + '/POSS3/getCasUrl').success(function(data) {
+                    $window.location.href = angular.fromJson(data) + '/login?service=' + encodeURIComponent($location.absUrl());
+                });
+            }
+            //Ticket validation and key application
+            else {
+                
+                var ticket = $location.search().ticket ;
+                $location.search('');
+                mrequest.do('POSS3', 'loginCas', {ticket: ticket, service: $location.absUrl()}).success(function(data){});
 
-        });
+                mrequest.do('POSS3', 'getStatus', {}).success(function(data){
+                    $scope.username = data.user_data.firstname +' '+ data.user_data.lastname;
+                    
+                    if(data.application == null){
+                        //No app key, we register one. appName to ask to user later
+                        var keyValue = localStorageService.get('applicationKey');
+                        if(keyValue == null){
+                            var appName = "dummyName";
+                            mrequest.do('KEY', 'registerApplication', {app_url: $location.absUrl(), app_name : appName}).success(function(data){
+                                localStorageService.add('applicationKey',data.app_key);
+                                mrequest.do('POSS3', 'loginApp', {key : localStorageService.get('applicationKey')}).success(function(data){
+                                    console.log(data);
+                                });
+                                                     
+                            });
+                        }
+                        else{
+                            mrequest.do('POSS3', 'loginApp', {key : localStorageService.get('applicationKey')}).success(function(data){
+                                console.log(data);
+                            });
+                        }
+                    }
+                    else{
+                    console.log("pouet");
+
+                    }
+                    });
 
         
-        }
+                
+            }
 
-    
-});
+
+         }
+
+);
     
     
