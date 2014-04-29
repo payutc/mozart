a724fdae0e8fbd71dedc078e52a56790827cbd77
Matthieu Guffroy <mattgu74@gmail.com>
2014-04-29 17:29:32 +0200

    Choix de la fundation uniquement quand nécessaire fix #41

--- js/app/controllers/fundation_controller.js
+++ js/app/controllers/fundation_controller.js
@@ -1,5 +1,4 @@
-mozartApp.controller('FunCtrl', function($scope, $http, mrequest){
-
+mozartApp.controller('FunCtrl', function($scope, $http, $modal, mrequest){
     $scope.$on("UPDATE_FUN",function(event,message){
          mrequest.do('POSS3',"getFundations").success(function(data){
              //No fundation
@@ -7,14 +6,30 @@ mozartApp.controller('FunCtrl', function($scope, $http, mrequest){
                 $scope.$emit("ERROR_FUN_RIGHTS","Aucune fundation trouvée. Vous n'avez pas de droits pour cette application");
              }
              else{
-                 //Constructing  HTML list of fundation
-                 $scope.fundations = data;
-                 $scope.funChoice = function(funId){
-                    $scope.funId = funId;
-                    $scope.fundations = null;
-                    $scope.$emit("MSG_GET_ARTICLES",$scope.funId);
-                 }
+                if(data.length == 1) {
+                    // Only one fundation, select it
+                    $scope.$emit("MSG_GET_ARTICLES",data[0].fun_id);
+                } else {
+                    if(data[0].fun_id == null) {
+                        data.shift();
+                    }
+                    $scope.fundations = data;
+
+                    var modalInstance = $modal.open({
+                      templateUrl: 'modalFunCtrl.html',
+                      controller: ModalFunCtrl,
+                      scope: $scope,
+                      keyboard: false
+                    });
+                }
              }
          });
     });
 });
+
+var ModalFunCtrl = function ($scope, $modalInstance) {
+  $scope.funChoice = function(funId){
+    $scope.$emit("MSG_GET_ARTICLES", funId);
+    $modalInstance.close();
+  }
+};
